9dcad978789d57434789063f45a9069f441b8674
Savo Lazic <savo.lazic@oicr.on.ca>
2018-03-09 12:10:25 -0500

    Added comments before I got too far behind

--- app.R
+++ app.R
@@ -1,38 +1,44 @@
 source("./utility.R")
 source("./list_reports.R")
 
-library(ggplot2)
 library(plotly)
 library(data.table)
 library(shiny)
 library(shinydashboard)
 
 # A data table that contains the paths and display names of the available Run Reports
+# The Run Reports should be ordered from oldest to newest
 all.runs.dt <- listRunReports()
 
-# This is the Run Report that will be displayed by the application
-# This global variable is polled by various functions and is updated when user selects different Run Reports
-# At startup this is the most recent Run Report
-current.dt <- all.runs.dt[nrow(all.runs.dt), ]
-
+# The UI
 ui <- dashboardPage(
   dashboardHeader(),
+  
+  # Most User Input Widgets are initialized dynamically by the server
   dashboardSidebar(
+    # Select Run Report
+    # Note that this assumes the oldest to newest ordering and displays the newest Run Report
     selectInput("run", "Select Run Report", sort(all.runs.dt$name, decreasing = TRUE)),
     
+    # Select the specific study within the Run Report (PCSI, CYT)
     selectInput("study", "Select Study", c()),
     
+    # Select Coverage cutoffs
     sliderInput("slider.coverage", "Coverage", 0, 0, value = c(0, 0)),
     
+    # Select which metrics (Map %, Coverage, % of Target) to plots
     checkboxGroupInput(
       "check.type", "Select Plots", c()
     )
   ),
   
+  #Displays the plots and any errors
   dashboardBody(
     # The css selector allows for the plot to be full height: https://stackoverflow.com/questions/36469631/how-to-get-leaflet-for-r-use-100-of-shiny-dashboard-height
     tags$style(type = "text/css", "#plot {height: calc(100vh - 80px) !important;}"),
     
+    # Shows any errors
+    # By default has no content and has no dimensions
     verbatimTextOutput("error_run"),
     
     # The css selector and the id of plotlyOutput must match
@@ -40,12 +46,20 @@ ui <- dashboardPage(
   )
 )
 
+# Server function that makes everything alive
 server <- function(session, input, output) {
+  # The current Run Report that is being displayed
+  # This Reactive Variable allows different watchers to access/modify the same information
+  # Can be set to NULL to display nothing. Useful if selected Run Report cannot be read
   current.run <- reactiveVal(NULL)
   
+  # Changing the Run Report updates the current.run and the Studies that can be selected
+  # If any error occurs in loading report, current.run is set to NULL (nothing will be plotted) and error message is set
   observeEvent(input$run, {
     tryCatch({
+      # Remove any previous error messages
       output$error_run <- renderPrint(invisible())
+      
       current.run(createAppDT(all.runs.dt[name == input$run, path]))
       all.studies <- names(current.run())
       updateSelectInput(session, "study", choices = all.studies)
@@ -56,6 +70,7 @@ server <- function(session, input, output) {
     })
   })
   
+  # Once a Run Report or Study is changed, update Coverage slider 
   observeEvent(c(input$run, input$study), {
     selected.study <- current.run()[[input$study]]
     req(selected.study)
@@ -72,6 +87,7 @@ server <- function(session, input, output) {
     }
   })
   
+  # Recalculates the data table to plot every time any variable within this expression is changed
   dt.to.plot <- reactive({
     req(input$run)
     req(input$check.type)
@@ -97,6 +113,7 @@ server <- function(session, input, output) {
     return(selected.study[type.to.keep])
   })
   
+  # Create the plot
   output$plot <- renderPlotly({
     req(input$check.type)
     
