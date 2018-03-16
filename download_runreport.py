0f7011c21ec526ffe6f5cc6c42ee594c5f8dc3ce
Savo Lazic <savo.lazic@oicr.on.ca>
2018-03-16 12:10:31 -0400

    Made Python Run Report download script more robust

--- download_runreport.py
+++ download_runreport.py
@@ -1,8 +1,62 @@
+#!/usr/bin/python3
+
 import glob
 import shutil
+import argparse
+import os.path
+import sys
+import os
 
-glob_path = "/mnt/HPC/oicr/data/archive/web/runReports/**/*_report.tsv"
+hpc_path = "/oicr/data/archive/web/runReports"
+hpc_glob = "**/*_report.tsv"
 
-for filename in glob.iglob(glob_path):
-    shutil.copy2(filename, "./runreport_files")
+parser = argparse.ArgumentParser(
+    description = """Obtain information about the Run Report TSV files
+    produced by SeqWare and hosted on the OICR HPC. By default prints all Run
+    Reports to stdout.""",
+    # Show the defaults if supplied
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+)
 
+parser.add_argument(
+    "-p", "--path", default = hpc_path,
+    help = "Path to HPC Run Report folder"
+)
+
+parser.add_argument(
+    "-g", "--glob", default = hpc_glob,
+    help = """The pathname pattern expansion used to list all Run Report files
+    using Python glob library"""
+)
+
+parser.add_argument(
+    "-s", "--save",
+    help = """Run Reports will be saved if path is specified"""
+)
+
+parser.add_argument(
+    "-f", "--force", action="store_true",
+    help = """Existing files and folders will be overwritten"""
+)
+
+arg_input = parser.parse_args()
+
+if arg_input.save is not None:
+    if os.path.exists(arg_input.save):
+        if arg_input.force:
+            shutil.rmtree(arg_input.save)
+        else:
+            print(
+                """{} cannot be created. If it already exists, use the -f flag
+                to overwrite the folder.""".format(arg_input.save),
+                file=sys.stderr
+            )
+            exit(1)
+
+    os.makedirs(arg_input.save)
+
+
+for filename in glob.iglob(os.path.join(arg_input.path, arg_input.glob)):
+    print(filename)
+    if arg_input.save is not None:
+        shutil.copy2(filename, arg_input.save)
