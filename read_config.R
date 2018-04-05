c9a2c03c6f2862af9394269e2f26e69e3f183d57
Savo Lazic <savo.lazic@oicr.on.ca>
2018-04-05 15:42:01 -0400

    Restructured config.yaml to give defaults their own heading

--- read_config.R
+++ read_config.R
@@ -2,8 +2,9 @@ library(yaml)
 
 CONFIG <- read_yaml("./config.yaml")
 CONFIG.RUNPATH <- CONFIG$app_instance$run_report_dir
-CONFIG.ALLPLOTS <- CONFIG$app_instance$all_plots
-CONFIG.DEFAULTPLOTS <- CONFIG$app_instance$default_plots
+CONFIG.ALLPLOTS <- CONFIG$app_instance$plots
+CONFIG.DEFAULTPLOTS <- CONFIG$default$plots
 CONFIG.PORT <- CONFIG$app_instance$port
 CONFIG.COLUMN <- CONFIG$app_instance$column_naming
 CONFIG.INFO.COLUMN <- CONFIG$app_instance$info_column
+CONFIG.DEFAULTORDER <- CONFIG$default$order
